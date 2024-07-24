ed752b88af8223e6d830c2a9c7760250663714fe
ardaeylen <ardaeylen@gmail.com>
2024-07-24 23:24:17 +0300

    Layer norm added on transformer blocks.

--- Block.py
+++ Block.py
@@ -11,10 +11,16 @@ class Block(nn.Module):
         self.context_len = context_len
         self.multihead_self_attention = MultiHeadAttention(embedding_size = self.embedding_size, num_heads = self.num_heads, context_length = self.context_len)
         self.feed_forward = FeedForward(emb_size = self.embedding_size)
+        # This layer norm actually is a per token normalization which normalizes every token
+        # along it's embedding dimension instead of batch dimension in Batch Normalization. Of
+        # course layer norm has gamma and beta trainable parameters the layer norm will eventually
+        # create outputs that might not be gaussian but the optimization will determine that.
+        self.layer_norm1 = nn.LayerNorm(self.embedding_size)
+        self.layer_norm2 = nn.LayerNorm(self.embedding_size)
 
     def forward(self, x):
-        x = x + self.multihead_self_attention(x)
-        x = x + self.feed_forward(x)
+        x = x + self.multihead_self_attention(self.layer_norm1(x))
+        x = x + self.feed_forward(self.layer_norm2(x))
         return x
 
 if __name__ == "__main__":
