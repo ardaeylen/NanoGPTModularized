ebc004db3b6d889dd55a8156bc99b56900bb3799
ardaeylen <ardaeylen@gmail.com>
2024-07-27 18:04:39 +0300

    Casual (own) implementation of MSA embedded into new Transformer Block that is embedded into new LanguageModel.

--- Block.py
+++ Block.py
@@ -1,7 +1,9 @@
 import torch.nn as nn
 from multihead_selfattention import MultiHeadAttention
+from self_attention import MultiHeadedSelfAttention
 from feedforwardlayer import FeedForward
 import torch
+
 
 class Block(nn.Module):
     def __init__(self, embedding_size, num_heads, context_len, dropout):
@@ -10,8 +12,9 @@ class Block(nn.Module):
         self.num_heads = num_heads
         self.context_len = context_len
         self.dropout = dropout
-        self.multihead_self_attention = MultiHeadAttention(embedding_size = self.embedding_size, num_heads = self.num_heads, context_length = self.context_len, dropout_rate=self.dropout)
-        self.feed_forward = FeedForward(emb_size = self.embedding_size, dropout_rate = self.dropout)
+        self.multihead_self_attention = MultiHeadAttention(embedding_size=self.embedding_size, num_heads=self.num_heads,
+                                                           context_length=self.context_len, dropout_rate=self.dropout)
+        self.feed_forward = FeedForward(emb_size=self.embedding_size, dropout_rate=self.dropout)
         # This layer norm actually is a per token normalization which normalizes every token
         # along it's embedding dimension instead of batch dimension in Batch Normalization. Of
         # course layer norm has gamma and beta trainable parameters the layer norm will eventually
@@ -23,6 +26,34 @@ class Block(nn.Module):
         x = x + self.multihead_self_attention(self.layer_norm1(x))
         x = x + self.feed_forward(self.layer_norm2(x))
         return x
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, context_length, embedding_size, query_dim, value_dim, num_heads, dropout_rate):
+        super(TransformerBlock, self).__init__()
+        self.embedding_size = embedding_size
+        self.num_heads = num_heads
+        self.context_len = context_length
+        self.dropout = dropout_rate
+        self.num_heads = num_heads
+        self.query_dim = query_dim
+        self.value_dim = value_dim
+        self.msa = MultiHeadedSelfAttention(context_length=self.context_len, embedding_size=self.embedding_size,
+                                            query_dim=self.query_dim, value_dim=self.value_dim,
+                                            num_heads=self.num_heads, dropout_rate=self.dropout)
+        self.feed_forward = FeedForward(emb_size=self.embedding_size, dropout_rate=self.dropout)
+        # This layer norm actually is a per token normalization which normalizes every token
+        # along it's embedding dimension instead of batch dimension in Batch Normalization. Of
+        # course layer norm has gamma and beta trainable parameters the layer norm will eventually
+        # create outputs that might not be gaussian but the optimization will determine that.
+        self.layer_norm1 = nn.LayerNorm(self.embedding_size)
+        self.layer_norm2 = nn.LayerNorm(self.embedding_size)
+
+    def forward(self, x):
+        x = x + self.msa(self.layer_norm1(x))
+        x = x + self.feed_forward(self.layer_norm2(x))
+        return x
+
 
 if __name__ == "__main__":
     block = Block(256, 16, 64)
--- model.py
+++ model.py
@@ -1,6 +1,46 @@
 import torch
 import torch.nn as nn
 from Block import Block
+from Block import TransformerBlock
+
+
+class LanguageModel(nn.Module):
+
+    def __init__(self, vocab_size, n_layer, emb_size=256, query_dim=256, value_dim=256, block_size=64, num_heads=16,
+                 dropout_rate=0.2):
+        super(LanguageModel, self).__init__()
+        # Each token directly reads off the logits for the next token from a lookup table
+        self.embedding_size = emb_size
+        self.vocab_size = vocab_size
+        self.block_size = block_size
+        self.query_dim = query_dim
+        self.value_dim = value_dim
+        self.num_heads = num_heads
+        self.layer_num = n_layer
+        self.dropout = dropout_rate
+        self.token_embedding_table = nn.Embedding(vocab_size, self.embedding_size)
+        self.position_embedding_table = nn.Embedding(self.block_size, self.embedding_size)
+        self.blocks = nn.Sequential(*[
+            TransformerBlock(context_length=self.block_size, embedding_size=self.embedding_size,
+                             query_dim=self.query_dim, value_dim=self.value_dim, num_heads=self.num_heads, dropout_rate=self.dropout) for _ in
+            range(self.layer_num)])
+        self.layer_norm = nn.LayerNorm(normalized_shape=self.embedding_size)  # Final layer norm.
+        self.lm_head = nn.Linear(self.embedding_size, self.vocab_size)
+
+    def forward(self, idx):
+        # idx is (B, T) tensor of integers.
+        # Now every single integer (token) in idx is mapped to corresponding embedding according to index of
+        # the given sample token in the vocabulary.
+        token_emb = self.token_embedding_table(idx)
+        # (Batch , Time (Sequence Length), C (Embedding Size))
+        pos_emb = self.position_embedding_table(torch.arange(0, self.block_size, device=torch.device("cuda")))
+        x = token_emb + pos_emb
+        x = self.blocks(x)
+        x = self.layer_norm(x)
+        logits = self.lm_head(x)  # outputs (Batch, Time (Sequence Length), Vocab Size)
+
+        return logits
+
 
 class BigramLanguageModel(nn.Module):
 
@@ -15,8 +55,9 @@ class BigramLanguageModel(nn.Module):
         self.dropout = dropout_rate
         self.token_embedding_table = nn.Embedding(vocab_size, self.embedding_size)
         self.position_embedding_table = nn.Embedding(self.block_size, self.embedding_size)
-        self.blocks = nn.Sequential(*[Block(self.embedding_size, self.num_heads, self.block_size, self.dropout) for _ in range(self.layer_num)])
-        self.layer_norm = nn.LayerNorm(normalized_shape=self.embedding_size) # Final layer norm.
+        self.blocks = nn.Sequential(
+            *[Block(self.embedding_size, self.num_heads, self.block_size, self.dropout) for _ in range(self.layer_num)])
+        self.layer_norm = nn.LayerNorm(normalized_shape=self.embedding_size)  # Final layer norm.
         self.lm_head = nn.Linear(self.embedding_size, self.vocab_size)
 
     def forward(self, idx):
@@ -35,8 +76,8 @@ class BigramLanguageModel(nn.Module):
 
 
 if __name__ == "__main__":
-    bigram_model = BigramLanguageModel(vocab_size = 65, n_layer = 3)
+    bigram_model = LanguageModel(vocab_size=65, n_layer=3)
     device = torch.device("cuda")
     bigram_model.to(device)
-    out = bigram_model(torch.randint(0, 65, (8, 64), device = torch.device("cuda")))
+    out = bigram_model(torch.randint(0, 65, (8, 64), device=torch.device("cuda")))
     print(out.shape)  # Now we got vocab size length embeddings for every (8,128) positions in the input sample.
